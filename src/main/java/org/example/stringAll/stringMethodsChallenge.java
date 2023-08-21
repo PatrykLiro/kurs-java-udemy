6471ccdc54fef564756a50eb275f6d3e1ad9bdb4
PatrykLiro <patrykliro8@gmail.com>
2023-08-21 15:38:42 +0200

    task with string methods

---
+++ src/main/java/org/example/stringAll/stringMethodsChallenge.java
@@ -0,0 +1,15 @@
+package org.example.stringAll;
+
+public class stringMethodsChallenge {
+    public static void main(String[] args) {
+        String txt = "Ola ma kota. ".repeat(3);
+        txt = txt.replaceAll("Ola", "Kasia");
+        txt = txt.replaceFirst("Kasia", "Monika");
+        System.out.println(txt);
+        int firstIndexOf = txt.indexOf("Kasia");
+        int lastIndexOf = txt.lastIndexOf("Kasia");
+        System.out.println(firstIndexOf);
+        System.out.println(lastIndexOf);
+        System.out.println(txt.substring(firstIndexOf));
+    }
+}
