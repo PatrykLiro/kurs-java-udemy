343a5089529fab9b5f36e2dc8ca2f9112e6dde34
PatrykLiro <patrykliro8@gmail.com>
2023-08-25 01:21:43 +0200

    task with return value

---
+++ src/main/java/org/example/oop/ReturnValueAndObject.java
@@ -0,0 +1,41 @@
+package org.example.oop;
+
+class Car {
+    String brand;
+    String model;
+    String color;
+
+    Car(String brand, String model, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+    public String getColor() {
+        return this.color;
+    }
+    public void printInfo() {
+        System.out.println(this.brand + " " + this.model + " " + this.color);
+    }
+}
+
+class CarFactory {
+    public Car buildCar() {
+        String colors[] = {"black", "white", "red"};
+        int random = (int) Math.floor(Math.random() * colors.length);
+        String randomColor = colors[random];
+        return new Car("Ford", "Mustang", randomColor);
+    }
+}
+
+public class ReturnValueAndObject {
+    public static void main(String[] args) {
+        CarFactory carFactory = new CarFactory();
+        Car car1 = carFactory.buildCar();
+        car1.printInfo();
+        System.out.println("Car1 color: " + car1.getColor());
+
+        Car car2 = carFactory.buildCar();
+        car2.printInfo();
+        System.out.println("Car2 color: " + car2.getColor());
+    }
+}
