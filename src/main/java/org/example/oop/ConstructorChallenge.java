5fa90bd38bcb2a26bc56c2365c9b3b0ab1c1ee1f
PatrykLiro <patrykliro8@gmail.com>
2023-08-25 00:07:58 +0200

    task with constructor

---
+++ src/main/java/org/example/oop/ConstructorChallenge.java
@@ -0,0 +1,32 @@
+package org.example.oop;
+
+class Student {
+    String name;
+    String surname;
+    String city;
+    int age;
+    public Student() {
+        this.name = "Jan";
+        this.surname = "Kowalski";
+        this.city = "Kraków";
+        this.age = 30;
+    }
+    Student(String name, String surname, String city, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.age = age;
+    }
+    public void printBasicData() {
+        System.out.println(this.name + " " + this.surname + " " + this.city + " " + this.age);
+    }
+}
+public class ConstructorChallenge {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.printBasicData();
+
+        Student student2 = new Student("Marek", "Nowak", "Warszawa", 18);
+        student2.printBasicData();
+    }
+}
