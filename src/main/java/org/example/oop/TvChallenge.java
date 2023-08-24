62881ebb212a6ba44b3a228d5cab787d7dda5f08
PatrykLiro <patrykliro8@gmail.com>
2023-08-24 13:37:31 +0200

    task with class and methos

---
+++ src/main/java/org/example/oop/TvChallenge.java
@@ -0,0 +1,21 @@
+package org.example.oop;
+
+class Tv {
+    String manufacture;
+    int year;
+    float inch;
+
+    public void tvInfo() {
+        System.out.println(this.manufacture + " " + year + " " + inch);
+    }
+}
+
+public class TvChallenge {
+    public static void main(String[] args) {
+        Tv tv = new Tv();
+        tv.manufacture = "Samsung";
+        tv.year = 2022;
+        tv.inch = 56.2f;
+        tv.tvInfo();
+    }
+}
