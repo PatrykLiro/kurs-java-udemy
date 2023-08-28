b419131fc6bc4d08e6ff4278a81a4c2b0a3e2239
PatrykLiro <patrykliro8@gmail.com>
2023-08-28 13:26:22 +0200

    dziedziczenie

---
+++ src/main/java/org/example/oop/FinalExtend.java
@@ -0,0 +1,51 @@
+package org.example.oop;
+
+class Employee {
+    String name;
+    String surname;
+    Employee() {
+
+    }
+    Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    public void printInfo() {
+        System.out.println("Name: " + name + " Surname: " + surname);
+    }
+}
+
+class Administrator extends Employee {
+    String certificates;
+    Administrator(String name, String surname, String certificates) {
+        this.name = name;
+        this.surname = surname;
+        this.certificates = certificates;
+    }
+    public void printInfo() {
+        System.out.println(this.name + " " + this.surname + " " + this.certificates);
+    }
+}
+
+class Programmer extends Employee {
+    String languages;
+    Programmer(String name, String surname, String languages) {
+        this.name = name;
+        this.surname = surname;
+        this.languages = languages;
+    }
+    public void printInfo() {
+        System.out.println(this.name + " " + this.surname + " " + this.languages);
+    }
+}
+
+public class FinalExtend {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Jan", "Kowalski");
+        employee.printInfo();
+        Programmer programmer = new Programmer("Marek", "Nowak", "Java");
+        programmer.printInfo();
+        Administrator administrator = new Administrator("Kasia", "Mirkowska", "Cert #1");
+        administrator.printInfo();
+    }
+}
