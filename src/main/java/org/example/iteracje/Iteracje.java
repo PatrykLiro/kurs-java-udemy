088c6808016184050ba0cd48e5036b9e9111d84c
PatrykLiro <patrykliro8@gmail.com>
2023-09-11 19:15:42 +0200

    arrayList iterator

---
+++ src/main/java/org/example/iteracje/Iteracje.java
@@ -0,0 +1,42 @@
+package org.example.iteracje;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Product {
+    protected String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
+public class Iteracje {
+    public static void main(String[] args) {
+        ArrayList<Product> products = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            products.add(new Product("Product #" + i));
+        }
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            System.out.println(product);
+        }
+
+    }
+}
